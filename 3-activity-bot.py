3fd763b7d13c3da46bc7535b55d241b3b76f2ef3
Roberto Moctezuma <rmoctezuma@gmail.com>
2019-07-07 14:29:58 -0500

    Uploading data to google sheet now

--- 3-activity-bot.py
+++ 3-activity-bot.py
@@ -8,14 +8,11 @@ This is a temporary script file.
 import pandas as pd
 import discord
 from datetime import datetime, timedelta
-from pytz import timezone
 import pytz
 import requests
-import numpy as np
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-#BOT_TOKEN = "NTg0ODE4NjA4ODU3OTM5OTc5.XP8kHw.Beigr96LwAu69_hxbBdhW5RzIIE"
 BOT_TOKEN = "NTg0ODM3NTg4NjQ1MzE0NTYz.XPQugQ.4-TLXdoVN0Ca84xaLo4kGoG7Bhk"
 
 def get_member(msg):
@@ -52,11 +49,41 @@ def get_clan_list():
     clan_df = pd.DataFrame(clan_data,columns = ['Name','Tag','ID','Platform','Key'])
     return clan_df
 
-def get_bungie_data(clan_id): #3007121
+def upload_clan(sh_title, clan_df):
+    df = clan_df.copy()
+    df.sort_values(by=['memberType','member'],ascending=[False,True],inplace=True)
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('google_keys.json', scope)
+    client = gspread.authorize(creds)
+    sheet = client.open("SGC Clans List")
+    try:
+        worksheet = sheet.worksheet(sh_title)
+        sheet.del_worksheet(worksheet)
+    except:
+        pass
+    worksheet = sheet.add_worksheet(title=sh_title, rows= str(len(df) + 10), cols="10")
+    cell_list = worksheet.range('A1:E' + str(len(df)+1))
+    cell_list[0].value = 'Discord name'
+    cell_list[1].value = 'Destiny display name'
+    cell_list[2].value = 'Discord active'
+    cell_list[3].value = 'Game active'     
+    cell_list[4].value = 'Member type'
+    row_counter = 0
+    for index,m in df.iterrows():
+        cell_list[row_counter*5 + 5].value = m.member
+        cell_list[row_counter*5 + 6].value = m.destinyDisplayName
+        cell_list[row_counter*5 + 7].value = m.discord_active
+        cell_list[row_counter*5 + 8].value = m.game_active        
+        cell_list[row_counter*5 + 9].value = m.memberType
+        row_counter = row_counter + 1
+    result = worksheet.update_cells(cell_list)
+    return result
+
+def get_bungie_data(clan_id):
     # Get the data!
     bungie_api = "https://www.bungie.net/Platform"
     api_key = "92fffbf33c304661b4766556741d0800"    
-    call = "/GroupV2/" + clan_id + "/Members/"
+    call = "/GroupV2/" + str(clan_id) + "/Members/"
     
     # Get the data from the API
     response = requests.get(bungie_api + call, headers =  { 'X-API-Key' : api_key })
@@ -69,13 +96,13 @@ def get_bungie_data(clan_id): #3007121
     clan = pd.DataFrame(results,columns = ['memberType','isOnline','lastOnlineStatusChange','groupId','destinyUserInfo',
                                            'bungieNetUserInfo','joinDate'])
     clan['destinyDisplayName'] = clan.apply(lambda x: x.destinyUserInfo['displayName'],axis=1)
-    clan['destinyMembershipType'] = clan.apply(lambda x: x.destinyUserInfo['membershipType'],axis=1)
-    clan['destinyMembershipId'] = clan.apply(lambda x: x.destinyUserInfo['membershipId'],axis=1)
-    clan['bungieSupplementalDisplayName'] = clan.apply(lambda x: x.bungieNetUserInfo['supplementalDisplayName'],axis=1)
-    clan['bungieIconPath'] = clan.apply(lambda x: x.bungieNetUserInfo['iconPath'],axis=1)
-    clan['bungieMembershipType'] = clan.apply(lambda x: x.bungieNetUserInfo['membershipType'],axis=1)
-    clan['bungieMembershipId'] = clan.apply(lambda x: x.bungieNetUserInfo['membershipId'],axis=1)
-    clan['bungieDisplayName'] = clan.apply(lambda x: x.bungieNetUserInfo['displayName'],axis=1)
+ #   clan['destinyMembershipType'] = clan.apply(lambda x: x.destinyUserInfo['membershipType'],axis=1)
+ #   clan['destinyMembershipId'] = clan.apply(lambda x: x.destinyUserInfo['membershipId'],axis=1)
+ #   clan['bungieSupplementalDisplayName'] = clan.apply(lambda x: x.bungieNetUserInfo['supplementalDisplayName'],axis=1)
+ #   clan['bungieIconPath'] = clan.apply(lambda x: x.bungieNetUserInfo['iconPath'],axis=1)
+ #   clan['bungieMembershipType'] = clan.apply(lambda x: x.bungieNetUserInfo['membershipType'],axis=1)
+ #   clan['bungieMembershipId'] = clan.apply(lambda x: x.bungieNetUserInfo['membershipId'],axis=1)
+ #   clan['bungieDisplayName'] = clan.apply(lambda x: x.bungieNetUserInfo['displayName'],axis=1)
     
     # Convert naive datetime to date-aware
     clan['lastOnline'] = pd.to_datetime(clan.lastOnlineStatusChange, unit = 's').dt.tz_localize('UTC')
@@ -83,15 +110,23 @@ def get_bungie_data(clan_id): #3007121
     # Convert to US Central
     clan['lastOnline'] = clan.lastOnline.dt.tz_convert('US/Central')
     right_now = pytz.utc.localize(datetime.utcnow()).astimezone(pytz.timezone('US/Central'))
-    clan['game_active'] = (right_now - clan.lastOnline).dt.days
-    clan = clan[['destinyDisplayName','memberType','lastOnline','game_active']]
+    clan['game_active'] = ((right_now - clan.lastOnline).dt.days <= 14)
+    clan = clan[['destinyDisplayName','memberType','game_active']]
     return clan
 
-def get_destiny_name(member_df, bungie_name):
-    m = member_df[member_df.member.str.contains(bungie_name)==True]
+def get_destiny_name(member_df, bungie_name, bungie_clan):
+    # Get the members with an exact match first
+    m = member_df[member_df.member == bungie_name]
+    if len(m)==0:
+        # No exact match, so try case insensitive
+        m = member_df[member_df.member.str.lower() == bungie_name.lower()]
+        if len(m) == 0:
+            # Still no match, try contains [but in same clan]
+            m = member_df[((member_df.member.str.lower().str.contains(bungie_name.lower())==True)
+                            & (member_df.discord_clan.str.lower() == bungie_clan.lower()))]
     if len(m) > 0:
         return m.member.iloc[0]
-    return 'Not found'
+    return None
 
 @client.event
 async def on_message(message):
@@ -140,21 +175,25 @@ async def on_message(message):
         listOfChannels = message.guild.text_channels
         for val in listOfChannels:
             await message.channel.send(str(val) + ' ' + str(val.position))
+    ##############################
+    #### !ALLACTIVITY
     if message.content.startswith('!allactivity'):
-        # Get the activity
-        activity_cutoff = datetime.now() - timedelta(days=14)
+        # Get the clan list
         clan_list = get_clan_list()
+        # Get a dataframe with the discord names - it's just one Discord server, set them all as Inactive now
         member_list = []
         for index,clan in clan_list.iterrows():
+            # The different clans are identified as roles
             print("Getting members of clan {}".format(clan.Tag))
-            role = discord.utils.get(message.guild.roles, name=clan['Tag'])
-            for member in role.members:
-                 member_data = { "clan" : clan['Tag'] , "member" :  member.display_name, "discord_active" : False }
-                 member_list.append(member_data)
-        member_df = pd.DataFrame(member_list, columns = ['clan', 'member', 'discord_active'])
-        member_df.member = member_df.member.str.lower()
+            discord_clan = discord.utils.get(message.guild.roles, name=clan.Tag)
+            if (not discord_clan is None):
+                for member in discord_clan.members:
+                     member_data = { "discord_clan" : clan.Tag , "member" :  member.display_name, "discord_active" : False }
+                     member_list.append(member_data)
+        member_df = pd.DataFrame(member_list, columns = ['discord_clan', 'member', 'discord_active'])
+        # Now loop through the messages, marking people who have messaged as Active
+        activity_cutoff = datetime.now() - timedelta(days=14)
         listOfChannels = message.guild.text_channels
-        i=0
         for channel in listOfChannels:
             try:
                 history = await channel.history(limit = 10000, after = activity_cutoff, oldest_first = False).flatten()
@@ -163,24 +202,49 @@ async def on_message(message):
                     member_df.loc[member_df.member == m.author.display_name,'discord_active'] = True
             except:
                 pass
-            i = i + 1
-            if (i>3):
-                break
+        # Lets now get the Bungie data
         print("Beginning Bungie data process")
         all_bungie_data = pd.DataFrame()
         for index,clan in clan_list.iterrows():
             print("Getting members of clan {} with Bungie ID # {}".format(clan.Tag,clan.ID))
             # Get Bungie info
-            bungie_info = get_bungie_data(clan['ID'])
+            bungie_info = get_bungie_data(clan.ID)
+            bungie_info['bungie_clan'] = clan.Tag
             # Merge them
-            bungie_info.destinyDisplayName = bungie_info.destinyDisplayName.str.lower()
-            bungie_info['discordName'] = bungie_info.apply(lambda x: 
-                    get_destiny_name(member_df,x.destinyDisplayName),axis=1)
+            bungie_info['discordName'] = bungie_info.apply(lambda x: get_destiny_name(member_df,x.destinyDisplayName, clan.Tag),axis=1)
             all_bungie_data = pd.concat([all_bungie_data,bungie_info],axis = 0, ignore_index = True, sort = False)
-        all_data = all_bungie_data.merge(member_df, how = 'outer', left_on='discordName', right_on='member')
+        
+        # Save to a CSV for debugging
+        all_bungie_data.to_csv('bungie.csv')
+        member_df.to_csv('discord.csv')
+        
+        # Merge them
+        all_data = all_bungie_data.merge(member_df, how = 'outer', left_on='discordName', right_on='member',indicator=True)
+        all_data['clan'] = all_data.apply(lambda x: x.bungie_clan if not pd.isnull(x.bungie_clan) else x.discord_clan, axis=1)
+        #all_data.drop(['discordName'],axis=1,inplace=True)
+        #all_data = all_data[['clan','member','destinyDisplayName','memberType','game_active','discord_active']]
+        
+        all_data.clan.fillna(value='[NONE]',axis=0,inplace=True)
+        all_data.member.fillna('',inplace=True)
+        all_data.destinyDisplayName.fillna('',inplace=True)
+        all_data.memberType.fillna(0,inplace=True)
+        all_data.discord_active.fillna(False,inplace=True)
+        all_data.game_active.fillna(False,inplace=True)
+        
         # Save the CSV
-        all_data.to_csv('activity-list.csv')
+        all_data.reset_index(drop=True,inplace=True)
+        all_data.to_csv('activity.csv')
+
+        for index,clan in clan_list.iterrows():
+            clan_data = all_data[all_data.clan == clan.Tag]
+            clan_data = clan_data[['member','destinyDisplayName','memberType','game_active','discord_active']]
+            upload_clan(clan.Tag, clan_data)
+        if len(all_data[all_data.clan == '[NONE]'])>0:
+            clan_data = all_data[all_data.clan == '[NONE]']
+            clan_data = clan_data[['member','destinyDisplayName','memberType','game_active','discord_active']]
+            upload_clan('[NONE]',clan_data)
         await message.channel.send("Done!")
+        
     if message.content.startswith('!roleactivity'):
         # Get the activity
         activity_cutoff = datetime.now() - timedelta(days=14)
