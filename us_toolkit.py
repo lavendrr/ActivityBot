d3b5c89c6a6af48c21e508dcac3bd489d494faeb
Ricardo Moctezuma <rmoctezumaf@gmail.com>
2020-08-11 11:18:59 -0500

    Updated Console Server ID

--- us_toolkit.py
+++ us_toolkit.py
@@ -132,7 +132,7 @@ def get_destiny_name(member_df, bungie_name, bungie_clan):
 async def update_sheets(run_mode, client):
     # Set the mode
     print('Running in mode {}.'.format(run_mode))
-    run_server = 100291727209807872 if run_mode == 'PC' else 614125874958303242
+    run_server = 100291727209807872 if run_mode == 'PC' else 742246399697092618
     # Get the channel for the staff log 
     log_channel = client.get_guild(STAFF_GUILD).get_channel(STAFF_CHANNEL)
     # Now get the guild to process
